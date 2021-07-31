75b46502161ef0bc1c84423a46f2861a236d1f8d
Gladibeth <gladibethdelosa@gmail.com>
2021-07-31 13:07:51 -0400

    Create project, Version desktop

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+WebFont.load({
+  google: {
+    families: ["Lato:300,400,500,700,900"],
+  },
+});
+
+
+var options = {
+  chart: {
+    height: 310,
+    type: "area",
+    zoom: {
+      enabled: false
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  series: [
+    {
+      name: "Days",
+      data: [2, 15, 10, 25, 30, 25, 28]
+    }
+  ],
+
+  fill: {
+    gradient: {
+      enabled: true,
+      opacityFrom: 0.55,
+      opacityTo: 0
+    }
+  },
+
+  tooltip: {
+    theme: "dark"
+  },
+  xaxis: {
+    categories: [
+      "Mon",
+      "Tue",
+      "Wed",
+      "Thu",
+      "Fri",
+      "Sat",
+      "Sun"
+    ]
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+chart.render();
